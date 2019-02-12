0bf86f804619fc2300f701636e3850bd3f14163b
KristinnVikarJ <kristinn@minecraftpalace.com>
2019-02-12 14:28:41 +0000

    Add files via upload

--- Markov.lua
+++ Markov.lua
@@ -1,13 +1,12 @@
 local m = {}
 m.__index = m
 
-local MarkovWeb = {}
-
 function ChooseWeighted(tbl) --i is string, v is number, has to be exactly that
 	local total = 0
 	for i,v in pairs(tbl) do
 		total = total + v
 	end
+	if total == 0 then return "" end
 	local rand = math.random(total)
 	for i,v in pairs(tbl) do
 		if rand <= v then
@@ -17,9 +16,9 @@ function ChooseWeighted(tbl) --i is string, v is number, has to be exactly that
 	end
 end
 
-function ChooseFirst()
+function ChooseFirst(self)
 	local tabl = {}
-	for i,v in pairs(MarkovWeb) do
+	for i,v in pairs(self.MarkovWeb) do
 		if v["StartChance"] then
 			tabl[i] = v["StartChance"]
 		end
@@ -42,12 +41,16 @@ function ProcessTable(tbl)
 end
 
 function m:Generate(minlength,maxlength,seed)
+	if self.Learned < 1 then
+		print("Cannot Generate with no learned data")		
+		return
+	end
 	local WordLength = math.random(minlength,maxlength)
-	local currentWord = seed or ChooseFirst()
+	local currentWord = seed or ChooseFirst(self)
 	local Sentence = currentWord
 
 	for i = 1, WordLength-1 do
-		local Processed = ProcessTable(MarkovWeb[currentWord])
+		local Processed = ProcessTable(self.MarkovWeb[currentWord])
 		
 		if not Processed then
 			break; --Sentence has no word to keep going so it has to stop :p
@@ -69,34 +72,36 @@ function m:Learn(text)
 	end
 	
 	if #words == 0 then return end
+	self.Learned = self.Learned + 1
 	
-	MarkovWeb[words[1]] = MarkovWeb[words[1]] or {}
+	self.MarkovWeb[words[1]] = self.MarkovWeb[words[1]] or {}
 
-	MarkovWeb[words[1]]["StartChance"] = MarkovWeb[words[1]]["StartChance"] == nil and 1 or MarkovWeb[words[1]]["StartChance"] + 1
-
+	self.MarkovWeb[words[1]]["StartChance"] = self.MarkovWeb[words[1]]["StartChance"] == nil and 1 or self.MarkovWeb[words[1]]["StartChance"] + 1
+	
 	for i = 1, #words do
 		local current = words[i]
 		local nxt = words[i+1]
 		
-		MarkovWeb[current] = MarkovWeb[current] or {}
+		self.MarkovWeb[current] = self.MarkovWeb[current] or {}
 		
 		if nxt then
-			MarkovWeb[current][nxt] = MarkovWeb[current][nxt] == nil and 1 or MarkovWeb[current][nxt] + 1
+			self.MarkovWeb[current][nxt] = self.MarkovWeb[current][nxt] == nil and 1 or self.MarkovWeb[current][nxt] + 1
 		end
 	end
 end
 
 function m:MapWeb() --Not recommended with a lot of data, might print thousands of lines of text
-	for i,v in pairs(MarkovWeb) do
+	for i,v in pairs(self.MarkovWeb) do
 		print(i,v," >")
 		for a,b in pairs(v) do
 			print("    >",a,b)
 		end
 	end
 end
+
 local module = {}
 function module:New()
-	return setmetatable({},{__index = m})
+	return setmetatable({MarkovWeb = {}, Learned = 0},{__index = m})
 end
 
 return module
