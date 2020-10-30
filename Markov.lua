fdc70d14a60763fa6e290222f0f24e4b78c194f0
Kristinn Vikar Jónsson <kristinn@minecraftpalace.com>
2020-10-30 22:41:47 +0000

    Fixed Issue
    Fixed Issue where text would continue generating forever, never ending with a chance like proper markov chains do

--- Markov.lua
+++ Markov.lua
@@ -1,7 +1,7 @@
 local Markov = {}
 Markov.__index = Markov
 
-function ChooseWeighted(tbl) --i is string, v is number, has to be exactly that
+function ChooseWeighted(tbl)
 	local total = 0
 	for i,v in pairs(tbl) do
 		total = total + v
@@ -19,8 +19,8 @@ end
 function Markov:ChooseFirst()
 	local tabl = {}
 	for i,v in pairs(self.MarkovWeb) do
-		if v.StartChance then
-			tabl[i] = v.StartChance
+		if v.__StartChance then
+			tabl[i] = v.__StartChance
 		end
 	end
 	return ChooseWeighted(tabl)
@@ -28,10 +28,10 @@ end
 
 function ProcessTable(tbl)
 	if next(tbl) == nil then return nil end
-	
+
 	local newtbl = {}
 	for i,v in pairs(tbl) do
-		if i ~= "nam" and i ~= "StartChance" then
+		if i ~= "nam" and i ~= "__StartChance" then
 			newtbl[i] = v
 		end
 	end
@@ -45,49 +45,58 @@ function Markov:Generate(minlength, maxlength, seed)
 	end
 	minlength = minlength or 1
 	maxlength = maxlength or 100
-	
+
 	local WordLength = math.random(minlength,maxlength)
 	local currentWord = seed or self:ChooseFirst()
 	local Sentence = currentWord
 
 	for i = 1, WordLength-1 do
 		local Processed = ProcessTable(self.MarkovWeb[currentWord])
-		
+
 		if not Processed then
 			break; --Sentence has no word to keep going so it has to stop :p
 		end
-		
+
 		local Word = ChooseWeighted(Processed)
+		
+		if Word == "__EndChance" then
+			break;
+		end
 		Sentence = Sentence .. " " .. Word
 		currentWord = Word
 	end
-	
+
 	return Sentence
 end
 
 function Markov:Learn(text)
 	if not text or string.len(text) == 0 then return end
 	local words = {}
-	
+
 	for word in text:gmatch("%w+") do
 		table.insert(words, word) 
 	end
-	
+
 	self.Learned = self.Learned + 1
-	
+
 	local Root = words[1]
-	
+
 	self.MarkovWeb[Root] = self.MarkovWeb[Root] or {}
-	self.MarkovWeb[Root].StartChance = self.MarkovWeb[Root].StartChance or 1
-	
+	self.MarkovWeb[Root].__StartChance = self.MarkovWeb[Root].__StartChance or 0
+
+	self.MarkovWeb[Root].__StartChance += 1
+
 	for i = 1, #words do
 		local current = words[i]
 		local nxt = words[i+1]
-		
+
 		self.MarkovWeb[current] = self.MarkovWeb[current] or {}
-		
+
 		if nxt then
 			self.MarkovWeb[current][nxt] = self.MarkovWeb[current][nxt] == nil and 1 or self.MarkovWeb[current][nxt] + 1
+		else
+			self.MarkovWeb[current].__EndChance = self.MarkovWeb[current].__EndChance or 0
+			self.MarkovWeb[current].__EndChance += 1
 		end
 	end
 end
